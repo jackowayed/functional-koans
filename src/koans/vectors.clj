cdff1072ace0e3febb8552af40ab8020614573b9
Daniel Jackoway <danjdel@gmail.com>
2010-08-22 04:25:20 -0400

    get started. do equalities, lists, vectors, sets.

--- src/koans/vectors.clj
+++ src/koans/vectors.clj
@@ -1,6 +1,6 @@
 (meditations
   "You can use vectors in clojure to create an 'Array' like structure"
-  (= __ (.size (vec nil)))
+  (= 0 (.size (vec nil)))
 
   "You can create a vector in two ways"
   (= [] (vec nil))
@@ -9,19 +9,19 @@
   (= [1] (vec '(1)))
 
   "But you can populate it with any number of elements at once"
-  (= [1 __] (vec '(1 2)))
+  (= [1 2] (vec '(1 2)))
 
   "And add to it as well"
-  (= __ (conj (vec nil) 333))
+  (= [333] (conj (vec nil) 333))
 
   "You can get the first element of a vector like so"
   (= :peanut (first [:peanut :butter :and :jelly]))
 
   "And the last in a similar fashion"
-  (= __ (last [:peanut :butter :and :jelly]))
+  (= :jelly (last [:peanut :butter :and :jelly]))
 
   "Or any index if you wish"
-  (= __ (nth [:peanut :butter :and :jelly] 3))
+  (= :jelly (nth [:peanut :butter :and :jelly] 3))
 
   "You can also slice a vector"
-  (= __ (subvec [:peanut :butter :and :jelly] 1 3)))
+  (= [:butter :and] (subvec [:peanut :butter :and :jelly] 1 3)))
